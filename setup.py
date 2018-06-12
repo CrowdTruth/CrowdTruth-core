19b06ced51f82345ba21d084aa8117e4c73dfc9f
ancadumitrache <anca.dumitrache@vu.nl>
2018-06-12 17:18:50 +0200

    correct code download url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 setup(name='crowdtruth',
-    version='2.0-alpha',
+    version='2.0-alpha.1',
     description="Disagreement based metrics for the processing and evaluation of crowdsourced annotations",
     long_description="CrowdTruth is an approach to machine-human computing for collecting annotation data on text, images and videos. The approach is focussed specifically on collecting gold standard data for training and evaluation of cognitive computing systems. The original framework was inspired by the IBM Watson project for providing improved (multi-perspective) gold standard (medical) text annotation data for the training and evaluation of various IBM Watson components, such as Medical Relation Extraction, Medical Factor Extraction and Question-Answer passage alignment.",
     classifiers=[],
@@ -11,7 +11,7 @@ setup(name='crowdtruth',
     author_email='crowdwatson@gmail.com',
     url='http://crowdtruth.org',
     license='Apache 2.0',
-    download_url = 'https://github.com/CrowdTruth/CrowdTruth-core/archive/v2.0-alpha.tar.gz',
+    download_url = 'https://github.com/CrowdTruth/CrowdTruth-core/archive/v2.0-alpha.1.tar.gz',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
